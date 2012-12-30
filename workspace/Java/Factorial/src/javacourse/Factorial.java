3cd158eff48d5c22cdea7138dbad1e0573cb61d1
The-Droidster <saurabh.khare1000@gmail.com>
2012-12-30 05:53:20 +0530

    Java -Factorial

---
+++ workspace/Java/Factorial/src/javacourse/Factorial.java
@@ -0,0 +1,23 @@
+package javacourse;
+
+import java.io.*; 
+
+class Factorial{
+  public static void main(String[] args) {
+  try{
+  BufferedReader object = new BufferedReader(
+  new InputStreamReader(System.in));
+  System.out.println("Enter the number:");
+  int a= Integer.parseInt(object.readLine());
+  double fact= 1;
+  System.out.println("Factorial of " +a+ ":");
+  for (int i= 1; i<=a; i++){
+  fact=fact*i;
+  }
+  System.out.println(fact);
+  }
+  catch (Exception e){
+  System.out.println("Array out of bounds exaception");
+  }
+  }
+}
