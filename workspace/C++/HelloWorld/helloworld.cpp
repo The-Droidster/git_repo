afbf23c4bd855ec31c5bfd063cd29812a56fd57c
The-Droidster <saurabh.khare1000@gmail.com>
2014-01-04 17:02:38 +0530

    Reconfig on new m/c, PHP added

---
+++ workspace/C++/HelloWorld/helloworld.cpp
@@ -0,0 +1,16 @@
+/*
+ * helloworld.cpp
+ *
+ *  Created on: 03-Jan-2014
+ *      Author: Saurabh Khare
+ */
+
+// my first program in C++
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World!";
+}
+
+
